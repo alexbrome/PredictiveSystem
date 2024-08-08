338e73d3d47b3b40f15cfc42c70a9f71dcd8f96c
Alejandro <123059423+alexbrome@users.noreply.github.com>
2024-08-08 11:14:32 +0200

    initial

---
+++ winepredictiveBack/src/main/java/com/winepredictive/winepredictive/WinepredictiveApplication.java
@@ -0,0 +1,13 @@
+package com.winepredictive.winepredictive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinepredictiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinepredictiveApplication.class, args);
+	}
+
+}
---
+++ winepredictiveBack/src/test/java/com/winepredictive/winepredictive/WinepredictiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winepredictive.winepredictive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinepredictiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
